48facc18645eab996e7415d1b17ea5c1deece38c
mshanemc <shane.mclaughlin@salesforce.com>
2019-11-16 16:34:32 -0800

    automation ftw

--- orgInit.sh
+++ orgInit.sh
@@ -3,6 +3,10 @@ sfdx shane:org:create -f config/project-scratch-def.json -d 1 -s -n --userprefix
 sfdx force:source:push
 sfdx force:user:permset:assign -n electron
 
+sfdx automig:load -d data
 sfdx shane:theme:activate -n Electron
-sfdx force:org:open
+sfdx force:org:open -p /lightning/o/Lead/list?filterName=All_Leads
 sfdx shane:user:password:set -g User -l User -p sfdx1234
+
+sfdx shane:heroku:repo:deploy -g gabesumner -r electron-website -n `basename "${PWD/mshanemc-/}"` -t autodeployed-demos
+sfdx shane:heroku:connect -a `basename "${PWD/mshanemc-/}"` -f config/heroku-connect-mappings.json -e custom
